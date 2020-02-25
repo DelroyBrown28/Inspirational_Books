d433ad0a51e2c89defb640da174dd56fc529c3ad
Delroy Brown <delroy_jb@hotmail.co.uk>
2020-02-25 22:53:18 +0000

    Drop down description box for 2nd book added

--- js/script.js
+++ js/script.js
@@ -1,12 +1,16 @@
 $(document).ready(function () {
 
     $(".bookBox-1").click(function () {
-        $(".descriptionBoxes").delay(500).slideToggle("medium");
+        $(".bookDescriptions-kinkyKenny").slideToggle();
+
     });
 
-    $(".bookBox-1").click(function () {
-        $(".main-title").slideToggle("medium");
-        $(".blurb").slideToggle("fast");
+
+    $(".bookBox-2").click(function () {
+        $(".bookDescriptions-eatWhatYoureGiven").slideToggle();
+ 
     });
+
+    
 
 });

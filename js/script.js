892efa014df16379d325c7d06573ccdce113596f
Delroy Brown <delroy_jb@hotmail.co.uk>
2020-02-25 14:15:25 +0000

    Javascript added for Description Box 1

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 $(document).ready(function () {
 
-    
-   
+    $(".bookBox-1").click(function () {
+        $(".descriptionBoxes").slideToggle("medium");
+    });
+
+    $(".bookBox-1").click(function () {
+        $(".main-title").slideToggle("medium");
+    });
 
 });

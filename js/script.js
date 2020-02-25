a1bc2599587a21dc2c8e4e68ae7f12197b680faa
Delroy Brown <delroy_jb@hotmail.co.uk>
2020-02-25 22:58:07 +0000

    Javascript updated

--- js/script.js
+++ js/script.js
@@ -2,15 +2,10 @@ $(document).ready(function () {
 
     $(".bookBox-1").click(function () {
         $(".bookDescriptions-kinkyKenny").slideToggle();
-
     });
-
 
     $(".bookBox-2").click(function () {
-        $(".bookDescriptions-eatWhatYoureGiven").slideToggle();
- 
+        $(".bookDescriptions-eatWhatYoureGiven").slideToggle(); 
     });
-
-    
 
 });

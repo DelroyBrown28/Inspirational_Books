b67f547deb2ddb9eb813c978022f1650679219c5
Delroy Brown <delroy_jb@hotmail.co.uk>
2020-02-25 14:28:28 +0000

    Javascript update

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 $(document).ready(function () {
 
     $(".bookBox-1").click(function () {
-        $(".descriptionBoxes").slideToggle("medium");
+        $(".descriptionBoxes").delay(500).slideToggle("medium");
     });
 
     $(".bookBox-1").click(function () {
         $(".main-title").slideToggle("medium");
+        $(".blurb").slideToggle("fast");
     });
 
 });

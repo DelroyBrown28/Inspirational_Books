f1f7d068ffcd0315d44f95842fdc1336312f6175
Delroy Brown <delroy_jb@hotmail.co.uk>
2020-02-27 19:19:58 +0000

    animated book boxes

--- js/script.js
+++ js/script.js
@@ -2,6 +2,9 @@ function slideUpFunction() {
     $(".main-title").slideUp();
     $(".blurb").slideUp();
     $(".bookDescriptions-iamprincess").delay(500).slideDown();
+    $(".bookBox-2, .bookBox-3, .bookBox-4, .bookBox-5").animate({
+        top: "800",
+    })
     $(".readNowbutton").delay(1000).animate({
         opacity: 1,
         width: "300",
@@ -14,8 +17,11 @@ function slideUpFunction() {
 function slideDownFunction() {
     $(".bookDescriptions-iamprincess").fadeOut();
     $(".main-title").delay(800).slideDown();
+    $(".bookBox-2, .bookBox-3, .bookBox-4, .bookBox-5").animate({
+        top: "0",
+    })
     $(".blurb").delay(500).slideDown();
-    $("body").css("background-color", "#A5B4BA")
+    $("body").css("background-color", "#A5B4BA", )
     $(".bookBox-1").unbind("click", slideDownFunction);
     $(".bookBox-1").click(slideUpFunction);
 }

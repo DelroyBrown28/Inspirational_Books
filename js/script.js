0fd0b3a10a4d2e40ec2e482187059c2423f12646
Delroy Brown <delroy_jb@hotmail.co.uk>
2020-02-27 18:48:44 +0000

    updated JQuery file

--- js/script.js
+++ js/script.js
@@ -2,11 +2,11 @@ function slideUpFunction() {
     $(".main-title").slideUp();
     $(".blurb").slideUp();
     $(".bookDescriptions-kinkyKenny").delay(500).slideDown();
-    $(".readNowbutton").delay("slow").animate({
-        delay: 13,
+    $(".readNowbutton").delay(1000).animate({
         opacity: 1,
-        
+        width: "300",
     });
+    $("body").css("background-color", "#8D999E")
     $(".bookBox-1").unbind("click", slideUpFunction);
     $(".bookBox-1").click(slideDownFunction);
 }
@@ -15,6 +15,7 @@ function slideDownFunction() {
     $(".bookDescriptions-kinkyKenny").slideUp();
     $(".main-title").delay(800).slideDown();
     $(".blurb").delay(500).slideDown();
+    $("body").css("background-color", "#A5B4BA")
     $(".bookBox-1").unbind("click", slideDownFunction);
     $(".bookBox-1").click(slideUpFunction);
 }

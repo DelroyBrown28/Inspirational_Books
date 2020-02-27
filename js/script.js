cc901208cbb1de3dcaacc7025b663a689e1ce647
Delroy Brown <delroy_jb@hotmail.co.uk>
2020-02-27 18:16:16 +0000

    Drop downs in place

--- js/script.js
+++ js/script.js
@@ -1,14 +1,23 @@
+function slideUpFunction() {
+    $(".main-title").slideUp();
+    $(".blurb").slideUp();
+    $(".bookDescriptions-kinkyKenny").delay(500).slideDown();
+    $(".readNowbutton").delay("slow").animate({
+        delay: 13,
+        opacity: 1,
+        
+    });
+    $(".bookBox-1").unbind("click", slideUpFunction);
+    $(".bookBox-1").click(slideDownFunction);
+}
+
+function slideDownFunction() {
+    $(".bookDescriptions-kinkyKenny").slideUp();
+    $(".main-title").delay(800).slideDown();
+    $(".blurb").delay(500).slideDown();
+    $(".bookBox-1").unbind("click", slideDownFunction);
+    $(".bookBox-1").click(slideUpFunction);
+}
 $(document).ready(function () {
-
-    $(".bookBox-1").click(function () {
-        $(".bookDescriptions-kinkyKenny").slideToggle();
-    });
-
-
-    $(".bookBox-2").click(function () {
-        $(".bookDescriptions-eatWhatYoureGiven").slideToggle();
-    });
-
-
-
+    $(".bookBox-1").click(slideUpFunction);
 });

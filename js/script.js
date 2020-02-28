d58b5c9edca565d46030ff07a7ef88e456e3dad3
Delroy Brown <delroy_jb@hotmail.co.uk>
2020-02-28 09:32:46 +0000

    synopsis update

--- js/script.js
+++ js/script.js
@@ -1,27 +1,50 @@
 $(document).ready(function () {
+
+    //BRINGS UP BOOK SYNOPSIS
+
     $(".bookBox-1").click(slideUpFunction);
+
+    function slideUpFunction() {
+        $(".main-title").slideUp();
+        $(".blurb").slideUp();
+        $(".bookDescriptions-iamprincess").delay(500).slideDown();
+        $(".readNowbutton").delay(1000).animate({
+            opacity: 1,
+            width: "300",
+        });
+        $(".bookBox-1").unbind("click", slideUpFunction);
+        $(".bookBox-1").click(slideDownFunction);
+    }
+
+    function slideDownFunction() {
+        $(".bookDescriptions-iamprincess").fadeOut();
+        $(".main-title").delay(800).slideDown();
+        $(".bookBox-2, .bookBox-3, .bookBox-4, .bookBox-5").animate({
+            top: "0",
+        });
+        $(".blurb").delay(500).slideDown();
+        $(".bookBox-1").unbind("click", slideDownFunction);
+        $(".bookBox-1").click(slideUpFunction);
+    }
+
+    //BRINGS UP FULL STORY
+
+
+    $(".readNowbutton").click(readNow);
+
+    function readNow() {
+        $(".bookBox-1, .bookBox-2, .bookBox-3, .bookBox-4, .bookBox-5").animate({
+            top: "800",
+        });
+        $(".readNowbutton").unbind("click")
+        $(".readNowbutton").click(readNowReverse);
+    }
+
+    function readNowReverse() {
+        $(".bookBox-1, .bookBox-2, .bookBox-3, .bookBox-4, .bookBox-5").animate({
+            top: "0",
+        });
+
+    }
+
 });
-
-function slideUpFunction() {
-    $(".main-title").slideUp();
-    $(".blurb").slideUp();
-    $(".bookDescriptions-iamprincess").delay(500).slideDown();
-    $(".readNowbutton").delay(1000).animate({
-        opacity: 1,
-        width: "300",
-    });
-    $(".bookBox-1").unbind("click", slideUpFunction);
-    $(".bookBox-1").click(slideDownFunction);
-}
-
-function slideDownFunction() {
-    $(".bookDescriptions-iamprincess").fadeOut();
-    $(".main-title").delay(800).slideDown();
-    $(".bookBox-2, .bookBox-3, .bookBox-4, .bookBox-5").animate({
-        top: "0",
-    });
-
-    $(".blurb").delay(500).slideDown();
-    $(".bookBox-1").unbind("click", slideDownFunction);
-    $(".bookBox-1").click(slideUpFunction);
-}
